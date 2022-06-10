639d8e10de7dc6f0bf0112f27b07160e321992c5
Ilia Filimonov <9991571155i@gmail.com>
2022-06-11 00:15:24 +0300

    Render added food

--- js/index.js
+++ js/index.js
@@ -15,6 +15,7 @@ const API = new FetchWrapper(
     "https://firestore.googleapis.com/v1/projects/jsdemo-3f387/databases/(default)/documents/YOURNAMESPACEHERE"
 );
 
+const list = document.querySelector("#food-list");
 const form = document.querySelector("#create-form");
 const name = document.querySelector("#create-name");
 const carbs = document.querySelector("#create-carbs");
@@ -38,9 +39,25 @@ form.addEventListener("submit", (event) => {
             return;
         }
 
+        list.insertAdjacentHTML(
+            "beforeend",
+            `<li class="card">
+          <div>
+            <h3 class="name">${name.value}</h3>
+            <div class="calories">0 calories</div>
+            <ul class="macros">
+              <li class="carbs"><div>Carbs</div><div class="value">${carbs.value}g</div></li>
+              <li class="protein"><div>Protein</div><div class="value">${protein.value}g</div></li>
+              <li class="fat"><div>Fat</div><div class="value">${fat.value}g</div></li>
+            </ul>
+          </div>
+        </li>`
+        );
+
         name.value = "";
         carbs.value = "";
         protein.value = "";
         fat.value = "";
     });
 });
+
